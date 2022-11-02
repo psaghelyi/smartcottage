daac558708980a4d969c90c4954f7619f71fa301
psaghelyi <psaghelyi@gmail.com>
2022-11-02 14:07:23 +0100

    prometheus & node_exporter

--- prometheus/start.sh
+++ prometheus/start.sh
@@ -5,6 +5,7 @@ ID=$(id -u)
 docker run -d \
     --restart unless-stopped \
     -p 9090:9090 \
+    --add-host host.docker.internal:host-gateway \
     --name prometheus \
     -v $PWD/.prometheus:/prometheus \
     -v $PWD/prometheus.yml:/etc/prometheus/prometheus.yml \
