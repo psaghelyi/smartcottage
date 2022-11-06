32703b729d934d659afd452664b1a6837d8058fc
psaghelyi <psaghelyi@gmail.com>
2022-11-06 22:37:29 +0100

    cAdvisor restart policy fixed

--- prometheus/exporters/cAdvisor/start.sh
+++ prometheus/exporters/cAdvisor/start.sh
@@ -2,14 +2,14 @@
 
 VERSION=v0.45.0
 
-sudo docker run \
+sudo docker run -d \
+  --restart unless-stopped \
   --volume=/:/rootfs:ro \
   --volume=/var/run:/var/run:ro \
   --volume=/sys:/sys:ro \
   --volume=/var/lib/docker/:/var/lib/docker:ro \
   --volume=/dev/disk/:/dev/disk:ro \
   --publish=8080:8080 \
-  --detach=true \
   --name=cadvisor \
   --privileged \
   --device=/dev/kmsg \
