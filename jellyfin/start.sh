3a508eab08eb7ccd3bd9c1f55545e2924024f1cc
psaghelyi <psaghelyi@gmail.com>
2022-11-07 12:34:23 +0100

    jellyfin DLNA ports added

--- jellyfin/start.sh
+++ jellyfin/start.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 docker run -d \
- --name jellyfin \
- --user $(id -u) \
- --publish=8096:8096 \
- --volume $PWD/.config:/config \
- --volume $PWD/.cache:/cache \
- --mount type=bind,source=/mnt/sda1/Media,target=/media \
- --restart=unless-stopped \
- jellyfin/jellyfin
+    --name jellyfin \
+    --user $(id -u) \
+    --publish=8096:8096 \
+    --publish=8920:8920 \
+    --publish=1900:1900/udp \
+    --publish=7359:7359/udp \
+    --volume $PWD/.config:/config \
+    --volume $PWD/.cache:/cache \
+    --mount type=bind,source=/mnt/sda1/Media,target=/media \
+    --restart=unless-stopped \
+    jellyfin/jellyfin
