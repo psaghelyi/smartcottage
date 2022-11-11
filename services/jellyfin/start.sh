cf43ae2ea8914fa0b1a8d0f15df091ba7026b7f7
psaghelyi <psaghelyi@gmail.com>
2022-11-11 18:35:14 +0100

    jellyfin DLNA

--- services/jellyfin/start.sh
+++ services/jellyfin/start.sh
@@ -3,12 +3,9 @@
 docker run -d \
     --name jellyfin \
     --user $(id -u) \
-    --publish=8096:8096 \
-    --publish=8920:8920 \
-    --publish=1900:1900/udp \
-    --publish=7359:7359/udp \
-    --volume $PWD/.config:/config \
-    --volume $PWD/.cache:/cache \
-    --mount type=bind,source=/mnt/sda1/Media,target=/media \
+    --net=host \
+    --volume jellyfin-config:/config \
+    --volume jellyfin-cache:/cache \
+    --volume /mnt/sda1/Media:/media \
     --restart=unless-stopped \
     jellyfin/jellyfin
