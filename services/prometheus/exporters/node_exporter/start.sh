8ced535877f7d3f2f8b66f212be4be655fa046ac
psaghelyi <psaghelyi@gmail.com>
2022-12-24 11:26:37 +0100

    switched node-exporter to Docker

---
+++ services/prometheus/exporters/node_exporter/start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+docker run -d \
+  --restart unless-stopped \
+  --name=node-exporter \
+  --net="host" \
+  --pid="host" \
+  -v "/:/host:ro,rslave" \
+  quay.io/prometheus/node-exporter:latest \
+  --path.rootfs=/host
