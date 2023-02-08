8d0e9aaace0a237f3ab0f61302002f0d9877d09c
psaghelyi <psaghelyi@gmail.com>
2023-02-08 20:43:11 +0100

    bitwarden added

---
+++ services/bitwarden/start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+docker run -d \
+    --name bitwarden \
+    --restart=unless-stopped \
+    --env-file $PWD/.env \
+    -v bitwarden-data:/data/ \
+    -p 127.0.0.1:8880:80 \
+    -p 127.0.0.1:3012:3012 \
+    vaultwarden/server:latest
--- services/nginx-reverse-proxy/start.sh
+++ services/nginx-reverse-proxy/start.sh
@@ -5,5 +5,6 @@ docker run -d \
   --network=host \
   -v $PWD/nginx.conf:/etc/nginx/nginx.conf \
   -v $PWD/.cert/bundle.pem:/etc/nginx/bundle.pem \
+  -v $PWD/.cert/dhparam.pem:/etc/nginx/dhparam.pem \
   --restart unless-stopped \
   nginx:latest
