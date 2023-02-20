43f23fb152cbd9363bf3679797f84ba2b5bdc7d2
psaghelyi <psaghelyi@gmail.com>
2023-02-20 16:53:01 +0100

    bitwarden port to 8083

--- services/bitwarden/start.sh
+++ services/bitwarden/start.sh
@@ -7,6 +7,6 @@ docker run -d \
     --env-file $PWD/.env \
     --user=$(id -u):$(id -g) \
     -v $PWD/.bitwarden-data:/data/ \
-    -p 127.0.0.1:8880:80 \
+    -p 127.0.0.1:8083:80 \
     -p 127.0.0.1:3012:3012 \
     vaultwarden/server:latest
