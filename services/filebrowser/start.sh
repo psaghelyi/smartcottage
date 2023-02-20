5ba1b3ca84c9f23689c91eead85b35f40806bfb5
psaghelyi <psaghelyi@gmail.com>
2023-02-20 12:26:29 +0100

    filebrowser

---
+++ services/filebrowser/start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+docker run -d \
+    --name filebrowser \
+    --pull=always \
+    --restart unless-stopped \
+    --user=$(id -u):$(id -g) \
+    -p 5555:80 \
+    -v /mnt/sda1:/srv \
+    -v $PWD/.filebrowser-data/filebrowser.db:/database/filebrowser.db \
+    -v $PWD/.filebrowser-data/.filebrowser.json:/.filebrowser.json \
+    filebrowser/filebrowser:latest
+
+
+    
