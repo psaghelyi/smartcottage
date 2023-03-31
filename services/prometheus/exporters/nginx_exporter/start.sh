548cec20663ccd604cf8607e4815c68da8c9abbc
psaghelyi <psaghelyi@gmail.com>
2023-03-31 14:28:20 +0200

    prometheus w/ docker compose

--- services/prometheus/exporters/nginx_exporter/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker run -d \
-  --restart unless-stopped \
-  --name=nginx-exporter \
-  --publish=9113:9113 \
-  --add-host host.docker.internal:host-gateway \
-  nginx/nginx-prometheus-exporter:0.10.0 \
-  '-nginx.scrape-uri=http://host.docker.internal:88/stub_status'
