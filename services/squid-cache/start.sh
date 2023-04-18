2be04439ffe9141c13bd127d789ac1aec353d937
psaghelyi <psaghelyi@gmail.com>
2023-04-18 21:53:23 +0200

    squid-cache

---
+++ services/squid-cache/start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+docker run -d \
+    --name squid-cache \
+    --pull=always \
+    --restart unless-stopped \
+    -p 3128:3128 \
+    --user=$(id -u):$(id -g) \
+    --log-driver=gelf \
+    --log-opt gelf-address=udp://localhost:12201 \
+    --log-opt tag=squid-cache \
+    -e TZ=Europe/Budapest \
+    -v $PWD/squid.conf:/etc/squid/squid.conf \
+    -v $PWD/.squid-data:/var/spool/squid \
+    -v $PWD/.squid-logs:/var/log/squid \
+    ubuntu/squid:latest \
+   
