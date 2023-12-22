92cf163b4f5cf05c62cde153b3263a3e1254d413
psaghelyi <psaghelyi@gmail.com>
2023-12-22 22:14:17 +0100

    zeronsd, code-server

--- services/code-server/start.sh
+++ services/code-server/start.sh
@@ -3,11 +3,15 @@
 docker run -d \
   --name=code-server \
   --pull=always \
-  --restart=always \
+  --restart unless-stopped \
   -e PUID=1000 \
   -e PGID=1000 \
   -e TZ=Europe/Budapest \
-  -p 8443:8443 \
-  -v code-server:/config \
+  -e PASSWORD=targa456 \
+  -e DEFAULT_WORKSPACE=/Project \
+  -p 9443:8443 \
+  -v $PWD/.config:/config \
   -v /mnt/sda1/Project:/Project \
   lscr.io/linuxserver/code-server:latest
+
+
--- services/zeronsd/start.sh
+++ services/zeronsd/start.sh
@@ -5,5 +5,5 @@ docker run -d \
     --restart=always \
     --network=host \
     -v /var/lib/zerotier-one/authtoken.secret:/authtoken.secret \
-    -v /home/pi/.token:/token.txt \
-    psaghelyi/zeronsd start -s /authtoken.secret -t /token.txt -d home.arpa d3ecf5726d92fd9c
+    -v /token.txt:/token.txt \
+    zerotier/zeronsd start -s /authtoken.secret -t /token.txt -d home.arpa d3ecf5726d92fd9c
