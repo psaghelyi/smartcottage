491f2946db264cc1cb5564729b8abbde629b5cbf
psaghelyi <psaghelyi@gmail.com>
2024-05-22 22:53:11 +0200

    fixed nginx reverse proxy and cert handling

--- services/nginx-reverse-proxy/gencert.sh
+++ services/nginx-reverse-proxy/gencert.sh
@@ -4,9 +4,12 @@ docker run -it --rm \
   --name=certbot-gencert \
   --network=host \
   --pull=always \
+  --user $(id -u):$(id -g) \
   -v $PWD/.config/certbot/www:/var/www/certbot:rw \
   -v $PWD/.config/certbot/conf:/etc/letsencrypt:rw \
-  certbot/certbot:arm64v8-latest certonly \
+  -v $PWD/.config/certbot/log:/var/log/letsencrypt:rw \
+  -v $PWD/.config/certbot/lib:/var/lib/letsencrypt:rw \
+  certbot/certbot:latest certonly \
   --webroot --webroot-path /var/www/certbot/ \
   -d psaghelyi.ddns.net \
   --email psaghelyi@hotmail.com
--- services/nginx-reverse-proxy/start.sh
+++ services/nginx-reverse-proxy/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker run -d \
-  --name=nginx-reverse-proxy \
+docker run \
+  --name=nginx-reverse-proxy -d \
   --pull=always \
   --restart=always \
   --network=host \
@@ -11,6 +11,5 @@ docker run -d \
   -v $PWD/nginx.conf:/etc/nginx/nginx.conf:ro \
   -v $PWD/nginx.conf.d:/etc/nginx/conf.d:ro \
   -v $PWD/.config/dhparam.pem:/etc/nginx/dhparam.pem:ro \
-  -v $PWD/.config/certbot/www:/var/www/certbot:ro \
   -v $PWD/.config/certbot/conf:/etc/nginx/ssl:ro \
   nginx:latest
